074d2a8bfdc387859d07feccf8020a8b4f014aac
Guyijoj <nguen.vin2005@mail.ru>
2025-10-15 22:55:14 +0300

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="p-3">
-      <div className="flex gap-2.5 h-15 items-center">
+      <div className="flex gap-2.5 h-15 items-center bg-orange-600">
         <Link className="auth-button " href="/sign-up">
           Sign up
         </Link>
