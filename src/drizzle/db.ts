02fee7d75e342e4425ede994e4a85763f7c54d35
Guyijoj <nguen.vin2005@mail.ru>
2025-10-23 00:51:22 +0300

    fix 1.5

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -5,7 +5,10 @@ import * as dotenv from "dotenv";
 import postgres from "postgres";
 
 dotenv.config();
-const client = postgres(process.env.DATABASE_URL!, { prepare: false });
+const client = postgres(process.env.DATABASE_URL!, {
+  ssl: "require",
+  prepare: false,
+});
 export const db = drizzle(client, {
   schema,
   logger: true,
